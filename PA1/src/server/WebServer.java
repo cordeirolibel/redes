61be34a17e7d4cd5e74be311473fa32f2f1613b7
cordeirolibel <gustavolibel@gmail.com>
2018-09-16 08:29:56 -0300

    first commit

---
+++ PA1/src/server/WebServer.java
@@ -0,0 +1,35 @@
+package server;
+
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+
+public final class WebServer
+{
+	public static void main(String argv[]) throws Exception
+	{
+		// Set the port number.
+		int port = 6789;
+		
+		// Establish the listen socket.
+		ServerSocket server = new ServerSocket(port);
+		
+		// Process HTTP service requests in an infinite loop.
+		while (true) {
+			
+			// Listen for a TCP connection request.
+			Socket client = server.accept();
+			
+			// Construct an object to process the HTTP request message.
+			HttpRequest request = new HttpRequest(client);//( ? );
+			// Create a new thread to process the request.
+			Thread thread = new Thread(request);
+			// Start the thread.
+			thread.start();
+		}
+
+		
+		
+		
+	}
+}
